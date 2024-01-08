da172a835acf5e91f26d8775c0559c1d4f31d043
Martin <120313968+MartinAdventure@users.noreply.github.com>
2024-01-08 20:10:22 +0100

    Add files via upload

---
+++ Cucumber JSON Server API Part 2/src/test/java/pl/akademiaqa/cucumber/steps/employee/CreateEmployeeSteps.java
@@ -0,0 +1,41 @@
+package pl.akademiaqa.cucumber.steps.employee;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import lombok.RequiredArgsConstructor;
+import pl.akademiaqa.api.employee.CreateEmployeeRequest;
+import pl.akademiaqa.context.Context;
+import pl.akademiaqa.dto.EmployeeDto;
+import pl.akademiaqa.handlers.employee.EmployeePayload;
+import pl.akademiaqa.handlers.employee.EmployeeResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor
+public class CreateEmployeeSteps {
+
+    private final CreateEmployeeRequest createEmployeeRequest;
+    private final EmployeePayload employeePayload;
+    private final Context context;
+
+    @When("I create new employee")
+    public void i_create_new_employee() {
+        createNewEmployee();
+    }
+
+    @Given("employee already exist")
+    public void employee_already_exist() {
+        createNewEmployee();
+    }
+
+    private void createNewEmployee() {
+        EmployeeDto defaultEmployee = employeePayload.getDefaultEmployee();
+        EmployeeResponse employeeResponse = createEmployeeRequest.createEmployee(defaultEmployee);
+
+        assertThat(employeeResponse.getFirstName()).isEqualTo(defaultEmployee.getFirstName());
+        assertThat(employeeResponse.getLastName()).isEqualTo(defaultEmployee.getLastName());
+        assertThat(employeeResponse.getEmail()).isEqualTo(defaultEmployee.getEmail());
+
+        context.setEmployeeResponse(employeeResponse);
+    }
+}
